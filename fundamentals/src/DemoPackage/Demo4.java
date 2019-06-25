ebde800b98bfa0684ddbfeaf552baccd777d0bc5
Admin <Admin@DESKTOP-T4F7246>
2019-06-25 10:48:31 +0530

    comit done

--- fundamentals/src/DemoPackage/Demo4.java
+++ fundamentals/src/DemoPackage/Demo4.java
@@ -2,7 +2,15 @@ package DemoPackage;
 
 public class Demo4 {
 	public static void main(String args[]) {
-		
+		if(Integer.parseInt(args[0])>0) {
+			System.out.println("Positive");
+		}
+		else if(Integer.parseInt(args[0])<0) {
+			System.out.println("Negative");
+		}
+		else {
+			System.out.println("Zero");
+		}
 	}
 
 }
